50fc359cc356cce697666f65255a6b21200ed61b
tomasz154 <tomasz154@10g.pl>
2016-11-05 19:24:01 +0100

    added Sorted

---
+++ src/Operation/Sorted.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace T2\Streams\Operation;
+
+
+use T2\Streams\Exception\InvalidArgumentException;
+use T2\Streams\Stream\ArrayStream;
+use T2\Streams\Stream\StreamInterface;
+use T2\Streams\TerminalOperation\ToArray;
+
+class Sorted implements StreamInterface
+{
+    private $originalStream;
+    private $sortedStream;
+
+    public function __construct(StreamInterface $stream)
+    {
+        if (!$stream->isBounded()) {
+            throw new InvalidArgumentException("Cannot sort unbounded stream");
+        }
+
+        $this->originalStream = $stream;
+    }
+
+    public function getCurrent()
+    {
+        return $this->getSortedStream()->getCurrent();
+    }
+
+    public function next()
+    {
+        return $this->getSortedStream()->next();
+    }
+
+    public function isBounded()
+    {
+        return true;
+    }
+
+    private function getSortedStream()
+    {
+        if (is_null($this->sortedStream)) {
+            $sorted = (new ToArray($this->originalStream))->getValue();
+            sort($sorted);
+            $this->sortedStream = new ArrayStream($sorted);
+        }
+
+        return $this->sortedStream;
+    }
+}
---
+++ tests/Operation/SortedTest.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace T2\Streams\Tests\Operations;
+
+
+use T2\Streams\Exception\EndOfStream;
+use T2\Streams\Exception\InvalidArgumentException;
+use T2\Streams\Operation\Sorted;
+use T2\Streams\Stream\ArrayStream;
+use T2\Streams\Stream\StreamInterface;
+
+class SortedTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNumbers()
+    {
+        $stream = new ArrayStream([2, 3, 1]);
+        $sorted = new Sorted($stream);
+
+        $this->assertEquals(1, $sorted->getCurrent());
+        $this->assertEquals(2, $sorted->getCurrent());
+        $this->assertEquals(3, $sorted->getCurrent());
+
+        $this->expectException(EndOfStream::class);
+        $sorted->getCurrent();
+    }
+
+    public function testStrings()
+    {
+        $stream = new ArrayStream(['q', 'w', 'e']);
+        $sorted = new Sorted($stream);
+
+        $this->assertEquals('e', $sorted->getCurrent());
+        $this->assertEquals('q', $sorted->getCurrent());
+        $this->assertEquals('w', $sorted->getCurrent());
+
+        $this->expectException(EndOfStream::class);
+        $sorted->getCurrent();
+    }
+
+    public function testEmptyStream()
+    {
+        $stream = new ArrayStream([]);
+        $sorted = new Sorted($stream, function ($item) {
+            return true;
+        });
+
+        $this->expectException(EndOfStream::class);
+        $sorted->getCurrent();
+    }
+
+    public function testUnbounded()
+    {
+        $stream = $this->prophesize(StreamInterface::class);
+        $stream->isBounded()->willReturn(false);
+
+        $this->expectException(InvalidArgumentException::class);
+        $reduce = new Sorted($stream->reveal(), 0, function ($carry, $item) {
+            return $carry + $item;
+        });
+    }
+}
