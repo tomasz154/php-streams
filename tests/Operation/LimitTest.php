045acc49b5a545545cfcdbca0a33d84d954c3a63
tomasz154 <tomasz154@10g.pl>
2016-11-05 18:56:09 +0100

    improved LimitTest

--- tests/Operation/LimitTest.php
+++ tests/Operation/LimitTest.php
@@ -7,6 +7,7 @@ namespace T2\Streams\Tests\Operations;
 use T2\Streams\Exception\EndOfStream;
 use T2\Streams\Operation\Limit;
 use T2\Streams\Stream\ArrayStream;
+use T2\Streams\Stream\Iterate;
 
 class LimitTest extends \PHPUnit_Framework_TestCase
 {
@@ -14,6 +15,8 @@ class LimitTest extends \PHPUnit_Framework_TestCase
     {
         $stream = new ArrayStream([1, 4, 10]);
         $limit = new Limit($stream, 2);
+
+        $this->assertTrue($limit->isBounded());
 
         $this->assertEquals(1, $limit->getCurrent());
         $this->assertEquals(4, $limit->getCurrent());
@@ -49,6 +52,25 @@ class LimitTest extends \PHPUnit_Framework_TestCase
     {
         $stream = new ArrayStream([]);
         $limit = new Limit($stream, 100);
+
+        $this->expectException(EndOfStream::class);
+        $limit->getCurrent();
+    }
+
+    public function testIterate()
+    {
+        $stream = new Iterate(10, function ($current) {
+            return $current + 1;
+        });
+        $limit = new Limit($stream, 4);
+
+        $this->assertFalse($stream->isBounded());
+        $this->assertTrue($limit->isBounded());
+
+        $this->assertEquals(10, $limit->getCurrent());
+        $this->assertEquals(11, $limit->getCurrent());
+        $this->assertEquals(12, $limit->getCurrent());
+        $this->assertEquals(13, $limit->getCurrent());
 
         $this->expectException(EndOfStream::class);
         $limit->getCurrent();
